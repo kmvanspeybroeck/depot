a3005e4b8250bc12304c261739799adc9a8bd861
Ken Van Speybroeck <kmvanspeybroeck@containerstore.com>
2013-12-10 13:24:50 -0600

    initial commit

---
+++ app/models/product.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  description :text
+#  image_url   :string(255)
+#  price       :decimal(8, 2)
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
+class Product < ActiveRecord::Base
+	validates :title, :description, :image_url, presence: true
+	validates :price, numericality:  {greater_than_or_equal_to: 0.01}
+	validates :title, uniqueness: true
+	validates :image_url, allow_blank: true, format: {
+		with:  %r{\.(gif|jpg|png)\Z}i,
+		message: 'must be a URL for GIF, JPG or PNG image.'
+	}
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '50a2437cd0ed8273376747394249a1046464a5013993ee799778d53c45ff60c0fa684f49457fdc2f1818d388cddb23f04dd6291251ab8fd6f9477b9ccf7c2a85'
