2585a364c36070cff49f386eadf114632b8dea0e
Ken Van Speybroeck <kmvanspeybroeck@containerstore.com>
2014-01-23 13:11:40 -0600

    Chap 11 Tests

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -21,8 +21,19 @@ class LineItemsControllerTest < ActionController::TestCase
       post :create, product_id: products(:ruby).id  #line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id }
     end
 
-    assert_redirected_to cart_path(assigns(:line_item).cart) #line_item_path(assigns(:line_item))
+    assert_redirected_to store_path
   end
+
+  test "should create line_item via ajax" do
+    assert_difference('LineItem.count') do
+      xhr :post, :create, product_id: products(:ruby).id
+    end
+    
+    assert_response :success
+    assert_select_jquery :html, '#cart' do
+      assert_select 'tr#current_item td', /Programming Ruby 1.9/
+    end
+  end      
 
   test "should show line_item" do
     get :show, id: @line_item
